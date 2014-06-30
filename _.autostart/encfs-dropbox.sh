19227ece956e7b720fbc3e8b85463da7b6f4f4a5
zcchen <chen.bill.bill@gmail.com>
2014-06-30 11:21:58 +0800

    Change the dropbox crypt path.

--- _.autostart/encfs-dropbox.sh
+++ _.autostart/encfs-dropbox.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 fusermount -u ~/Documents/Dropbox/
-encfs ~/Documents/.DropBox-crypt/Dropbox/ ~/Documents/Dropbox/ \
+encfs ~/Documents/Dropbox/.crypt/ ~/Documents/Dropbox/decrypt_dir/ \
     --extpass="cat ~/.myPasswd/encfs-dropbox.txt"
